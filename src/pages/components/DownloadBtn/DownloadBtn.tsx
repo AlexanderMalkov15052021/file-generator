463b21810597801173102a8b795708bcbccc7e14
Alex Malkov <malkov.fighter@yandex.ru>
2024-08-27 20:13:19 +0700

    Initialize project

---
+++ src/pages/components/DownloadBtn/DownloadBtn.tsx
@@ -0,0 +1,17 @@
+import { Button } from "antd";
+import Link from "antd/lib/typography/Link";
+import { memo } from "react";
+
+const DownloadBtn = ({ href }: { href: string | null }) => {
+    return <>
+        <Button className="buttun-upload" disabled={href ? false : true} type={"primary"}>
+            <Link
+                href={`${href ? href : ""}`} download={"domodedovo.building.html"}
+            >
+                Скачать .mooe
+            </Link>
+        </Button>
+    </>
+}
+
+export default memo(DownloadBtn);
