f71322481581924c44c0d384cf5e6fac74d7a8c7
Alex Malkov <malkov.fighter@yandex.ru>
2024-10-03 16:11:21 +0700

    Fix cache point dist

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 const distBetweenStackAndCachePoint = 0.15;
 
-export const fromStackToCachePoint = 1.2;  // от палеты до края
+export const fromStackToCachePoint = 1.5;  // от палеты до края
 export const fromStackToEnd = 0.16;  // от палеты до края
 export const distSmallRoad = 2.16;  // длинна маленткой дороги
 export const fromStackToTargetPoint = 0.6;  // от палеты до точки заезда
