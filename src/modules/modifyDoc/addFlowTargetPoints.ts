8106de5b1a732352a47a27b0a40b6fd244af6540
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-05-30 21:51:19 +0700

    fix/create_target_points - add all target points to flow

--- src/modules/modifyDoc/addFlowTargetPoints.ts
+++ src/modules/modifyDoc/addFlowTargetPoints.ts
@@ -1,14 +1,14 @@
-import { distCenterToTargetPoint, roadsDist } from "@/constants";
+import { distCenterToTargetPoint } from "@/constants";
 import { getAtan2 } from "@/helpers/math";
 import { Coords, MooeDoc } from "@/types";
 import { targetPoint } from "@/helpers/points/targetPoint";
 import { GeneratorStor } from "@/entities";
 import { getPointIdsBuffer } from "./getPointIdsBuffer";
 
-export const addFlowTargetPoints = (mooeDoc: MooeDoc, newPoints: Coords[], sideAngle: number, innerFlow?: boolean) => {
+export const addFlowTargetPoints = (mooeDoc: MooeDoc, newPoints: Coords[], sideAngle: number, names: string[][], innerFlow?: boolean) => {
 
     const {
-        store: { lastFlowNum, namingOrder, dirRoad },
+        store: { dirRoad },
     } = GeneratorStor;
 
     const angle = getAtan2(newPoints[0].x, newPoints[0].y, newPoints[newPoints.length - 1].x, newPoints[newPoints.length - 1].y);
@@ -25,21 +25,22 @@ export const addFlowTargetPoints = (mooeDoc: MooeDoc, newPoints: Coords[], sideA
         const pointX = Math.cos(angle) * (distCenterToTargetPoint / 2 * targetPointDir) + points.x;
         const pointY = Math.sin(angle) * (distCenterToTargetPoint / 2 * targetPointDir) + points.y;
 
-        const columnName = namingOrder === 1 ? String(newPoints.length - index) : String(index + 1);
-        const targetColumnName = columnName.length === 1 ? `0${columnName}` : columnName;
+        // Добавление таргет поинтов
+        names.map((arr, i) => {
 
-        mooeDoc?.mLaneMarks.push(targetPoint(
-            pointIdsBuffer[index],
-            pointX,
-            pointY,
-            pointAngle,
-            lastFlowNum,
-            targetColumnName,
-            String(roadsDist.length),
-            "前置点",
-            targetPointDir,
-            "GT"
-        ));
+            const name = arr[index];
+
+            mooeDoc?.mLaneMarks.push(targetPoint(
+                pointIdsBuffer[i + (index * names.length)],
+                pointX,
+                pointY,
+                pointAngle,
+                name,
+                "前置点",
+                targetPointDir
+            ));
+        });
+
     });
 
 }
